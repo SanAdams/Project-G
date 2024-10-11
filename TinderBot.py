7b1da49abc27535dd1bafcca187e4226c64504a4
SanAdams <sebastien1adam@gmail.com>
2024-10-11 15:25:07 -0700

    Add logic for scrape_basics()

--- TinderBot.py
+++ TinderBot.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
-import os
+from collections import defaultdict
 import time
 import DatingAppUser
 
@@ -54,7 +54,47 @@ def scrape_relationship_type():
         By.XPATH, '//*[@id="q1029118820"]/div/div[1]/div/main/div[1]/div/div/div/div[1]/div[1]/div/div[2]/div[4]/div/div/div/div')
     return relationship_type
 
+def scrape_basics():
+    # Make default value of any key a ""
+    basic_attributes = defaultdict(lambda: "")
+    try:
+        basics_spans = driver.find_elements(By.XPATH, '//h2[text()="Basics"]//following-sibling::div//div//span')
+    except NoSuchElementException:
+        return basic_attributes
+    # Match the attribute text to the attribute
+    if basics_spans:  
+        for span in basics_spans:
+            if span.text.strip() == "Education":
+                basic_attributes['education_level'] = span.find_element(By.XPATH, './following-sibling::div').text
+            if span.text.strip() == "Love Style":
+                basic_attributes['love_language'] = span.find_element(By.XPATH, './following-sibling::div').text
+            if span.text.strip() == "Communication Style":
+                basic_attributes['communication_style'] = span.find_element(By.XPATH, './following-sibling::div').text
+            if span.text.strip() == "Zodiac":
+                basic_attributes['star_sign'] = span.find_element(By.XPATH, './following-sibling::div').text
+            if span.text.strip() == "Family Plans":
+                basic_attributes['family_plans'] = span.find_element(By.XPATH, './following-sibling::div').text
+            if span.text.strip() == "COVID Vaccine":
+                basic_attributes['vaccination_status'] = span.find_element(By.XPATH, './following-sibling::div').text
+            if span.text.strip() == "Personality Type":
+                basic_attributes['personality_type'] = span.find_element(By.XPATH, './following-sibling::div').text
+    return basic_attributes
 
+def scrape_lifestyle():
+    lifestlye_attributed = {lambda: ""}
+    
+def scrape_interests():
+    pass
+
+def scrape_top_spotify_artists():
+    pass
+
+def scrape_anthem():
+    pass
+
+def scrape_languages():
+    pass
+#TODO: Change the time.sleep waits into explicit waits from selenium
 if __name__ == '__main__':
     NUM_PROFILES = 1
 
