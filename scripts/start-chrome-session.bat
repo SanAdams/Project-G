ee5051f1f489305a3fb4b3014d7f482796204ba6
Sebastien <sebastien1adam@gmail.com>
2024-10-19 01:06:23 -0700

    Minor bug fix with paths

--- scripts/start-chrome-session.bat
+++ scripts/start-chrome-session.bat
@@ -1,11 +1,10 @@
-start-chrome-session
 @echo off
 
 title TestCMD
 echo Starting chrome session...
 
 :: Open chrome in debugging mode
-start "" "..\BE\dependencies\chrome-win64\chrome-win64\chrome.exe" --remote-debugging-port=9222
+start "" "..\BE\dependencies\chrome-win64\chrome.exe" --remote-debugging-port=9222
 
 :: Wait for chrome to start up
 "%systemroot%\System32\timeout.exe" /t 1 >nul
