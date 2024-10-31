2e9015b1af680575436e37c434feb6ff312ee54c
SanAdams <sebastien1adam@gmail.com>
2024-10-31 05:53:51 -0700

    Added a more consistent wait

--- scripts/start-chrome-session.bat
+++ scripts/start-chrome-session.bat
@@ -6,8 +6,13 @@ echo Starting chrome session...
 :: Open chrome in debugging mode
 start "" "..\BE\dependencies\chrome-win64\chrome.exe" --remote-debugging-port=9222
 
-:: Wait for chrome to start up
-"%systemroot%\System32\timeout.exe" /t 1 >nul
+:: Check if Chrome is loaded
+:checkChrome
+tasklist /FI "IMAGENAME eq chrome.exe" 2>NUL | find /I "chrome.exe" >NUL
+if %ERRORLEVEL% neq 0 (
+    timeout /t 1 >nul
+    goto checkChrome
+)
 
 :: Bring cmd back to foreground
 "%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe" -command "(new-object -comobject wscript.shell).AppActivate('TestCMD') > $nul"
