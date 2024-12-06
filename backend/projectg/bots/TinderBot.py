dbb643110455da60e2f6415212b43eb532de8b60
Sebastien Adam <sebastien1adam@gmail.com>
2024-12-05 22:31:35 -0800

    Added functionality to scrape relationship type and intent
    Implemented get_div_indexes

--- backend/projectg/bots/TinderBot.py
+++ backend/projectg/bots/TinderBot.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from typing import Optional
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
@@ -7,65 +8,79 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from .BaseScraper import BaseScraper
 from selenium.webdriver.common.keys import Keys
+from projectg.models.Prompt import Prompt
 class TinderBot(BaseScraper):
 
-    def get_section_div_idx(self, section_title: str) -> Optional[int]:
-        # sections_titles = [
-        #     'essentials',
-        #     'basics',
-        #     'relationship details',
-        #     'basics',
-        #     'lifestyle',
-        #     'interests',
-        #     'about me'
-        # ]
+    def get_div_indexes(self) -> dict[str, Optional[int]]:
+        """
+        Returns a dictionary dict[str, Optional[int]] that maps the section title to the index of its div if the div exists.
+        """
 
-        sections = self.driver.find_elements(By.CLASS_NAME, 
+        div_indexes = defaultdict(lambda: None)
+
+        divs = self.driver.find_elements(By.XPATH, 
                                   "//div[contains(@class, 'P(24px)') and contains(@class, 'W(100%)') and contains(@class, 'Bgc($c-ds-background-primary)') and contains(@class, 'Bdrs(12px)')]"
                                   )
+
         
-        for i in range(len(sections)):
-            section = sections[i]
-            if section.find_element()
+        for i in range(len(divs)):
+            current_div = divs[i]
+            heading = current_div.find_element(By.XPATH, ".//div[contains(@class, 'Mstart(8px)') and contains(@class, 'Mb(0)') and contains(@class, '$c-ds-text-secondary') and contains(@class,'Typs(body-2-strong)')]")
+            # Query as to whether the index as provided by this method will be correct in the XPATH or if it's off by 1
+            div_indexes[f'{heading.text}'] = i
 
-        idx: int
+        return div_indexes
 
-
-        return idx
-    def open_profile(self):
+    def open_profile(self) -> None:
         body = self.driver.find_element(By.TAG_NAME, 'body')
         body.send_keys(Keys.ARROW_UP)
 
 
-    def scrape_name(self):
+    def scrape_name(self) -> str:
         names = self.driver.find_elements(By.XPATH, '//span[@class="Typs(display-1-strong)"]')
         return names[1].text
 
 
-    def scrape_age(self):
+    def scrape_age(self) -> int:
         ages = self.driver.find_elements(
             By.XPATH, '//span[@class="Typs(display-2-regular) As(b)"]//span')
         return int(ages[1].text)
     
 
-    def next_profile(self):
+    def next_profile(self) -> None:
         body = self.driver.find_element(By.TAG_NAME, 'body')
         body.send_keys(Keys.ARROW_LEFT)
 
 
-    def scrape_relationship_intent(self):
-        self.driver.find_element(By.XPATH, '//div[text() = "Looking for"]')
-        
+    def scrape_relationship_intent(self) -> str:
+        relationship_span_xpath = "//span[contains(@class, 'Typs(display-3-strong)') and contains(@class, 'C($c-ds-text-primary)') and contains(@class, 'Mstart(4px)')]"
+        span = self.driver.find_element(By.XPATH, relationship_span_xpath)
+        return span.text
+
+    def scrape_bio(self) -> Optional[str]:
+        pass
 
     def scrape_relationship_type(self) -> Optional[str]:
-        try:
+        relationship_types = [
+            'Open to exploring',
+            'Ethical non-monogamy',
+            'Monogamy',
+            'Open relationship',
+            'Polyamory',
+        ]
 
-        except NoSuchElementException:
-            self.logger.error('Relationship type element not properly found')
-            return None
+        for relationship_type in relationship_types:
+            try:
+                self.driver.find_element(By.XPATH, f"//div[text() = '{relationship_type}']")
+                return relationship_type
+            except NoSuchElementException:
+                pass
         
+        return None
 
     def scrape_basics(self):
+
+        #TODO: Rewrite this entire function. It doesn't work
         basic_attributes = defaultdict(lambda: "")
 
         try:
@@ -163,29 +178,39 @@ class TinderBot(BaseScraper):
         return lifestyle_attributes
 
 
-    def scrape_interests():
+    def scrape_interests(self):
         pass
 
 
-    def scrape_top_spotify_artists():
+    def scrape_top_spotify_artists(self):
         pass
 
 
-    def scrape_essentials():
+    def scrape_essentials(self):
         pass
 
 
-    def scrape_languages():
+    def scrape_languages(self):
         pass
 
-    def scrape_anthem():
+    def scrape_anthem(self):
         pass
     
     def scrape_profile(self):
+        div_indexes = self.get_div_indexes()
+
         print(self.scrape_name())
         print(self.scrape_age())
+
         self.open_profile()
+        print(self.scrape_relationship_intent())
+
         print(self.scrape_relationship_type())
+        
+        # print(self.scrape_bio())
+
         # print(self.scrape_basics())
+        
+        # print(div_indexes)
+        
         # print(self.scrape_lifestyle())
-        # self.next_profile()
